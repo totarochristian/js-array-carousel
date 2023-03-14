eef690f02298a23ba062571fa4b4fb5c728a61b9
totarochristian <totaro.christian.18@gmail.com>
2023-03-14 15:48:34 +0100

    Add comments to the fun ctions defined in the utility script file

--- js/utility.js
+++ js/utility.js
@@ -1,7 +1,13 @@
+/**
+ * Function used to update the image showed (as active) in the slider.
+ */
 function UpdateSlideShowed(){
     document.getElementById("sliderImg").setAttribute("src","./assets/img/"+sliderImages[sliderCurrentIndex]);
 }
 
+/**
+ * Function used to decrease the index of the active image in the slider, and when arrive to 0 go to the last image of the array.
+ */
 function PrevSlide(){
     if(sliderCurrentIndex>0)
         sliderCurrentIndex--;
@@ -10,6 +16,9 @@ function PrevSlide(){
     UpdateSlideShowed();
 }
 
+/**
+ * Function used to increase the index of the active image in the slider, and when arrive to the last element go to the first image of the array.
+ */
 function NextSlide(){
     if(sliderCurrentIndex<(sliderImages.length-1))
         sliderCurrentIndex++;
